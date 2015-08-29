3cf1f74c2e93848517696e62764320628d6d827d
Sukhanov Stas <stas.sukhanov@gmail.com>
2015-08-29 20:41:30 +0500

    Script versions. Added default script to easy start.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,6 @@
 (function () {
 
-  $('#user_view').val(localStorage.getItem('user_view'));
-  $('#user_data').val(localStorage.getItem('user_data'));
+  const SCRIPT_VERSION = "0.0.1";
 
   var fn = {};
 
@@ -13,6 +12,82 @@
 
     $('.tab-view').removeClass('active')
     $('#' + active.data('view')).addClass('active');
+
+  };
+
+  fn.fillFromFile = function (field, url, ifEmpty) {
+
+    var f = $(field)[0];
+    if (!ifEmpty || !f.value) {
+      var r = new Request(url);
+      fetch(r).then(function (response) {
+        return response.text();
+      }).then(function (text) {
+        f.value = text;
+      });
+    }
+
+  };
+
+  fn.prepare = function () {
+
+    fn.fillFromFile($('#user_view'), 'default.html', true);
+    fn.fillFromFile($('#user_data'), 'scripts/data.js', true);
+
+  };
+  
+  fn.reset = function () {
+
+    fn.fillFromFile($('#user_view'), 'default.html');
+    fn.fillFromFile($('#user_data'), 'scripts/data.js');
+
+  };  
+
+  fn.updatePreview = function () {
+
+    var view = $('#preview');
+    var doc = view[0].contentDocument;
+    var win = view[0].contentWindow;
+    var userView = $('#user_view')[0];
+    var userData = $('#user_data')[0];
+
+    if (userData.value !== null) {
+      win.eval(userData.value);
+    }
+
+    if (userView.value !== null) {
+      doc.body.innerHTML = userView.value;
+      doc.dispatchEvent(new Event('WebComponentsReady'));
+    }
+
+  };
+
+  fn.storeState = function () {
+
+    var userView = $('#user_view')[0];
+    var userData = $('#user_data')[0];
+
+    localStorage.setItem('version', SCRIPT_VERSION);
+    localStorage.setItem('user_view', userView.value);
+    localStorage.setItem('user_data', userData.value);
+
+  };
+
+  fn.restoreState = function () {
+
+    var version = localStorage.getItem('version');
+
+    if (!version) {
+      return;
+    }
+
+    if (version !== SCRIPT_VERSION) {
+      alert("Script format was changed. Saved script cannot be restored.");
+      return;
+    }
+
+    $('#user_view').val(localStorage.getItem('user_view'));
+    $('#user_data').val(localStorage.getItem('user_data'));
 
   };
 
@@ -28,25 +103,23 @@
   });
 
   $('#run').click(function () {
-
-    var view = $('#preview');
-    var doc = view[0].contentDocument;
-    var win = view[0].contentWindow;
-    var userView = $('#user_view');
-    var userData = $('#user_data');
-
-    localStorage.setItem('user_view', userView.val());
-    localStorage.setItem('user_data', userData.val());
-
-    win.eval(userData.val());
-    
-    doc.body.innerHTML = userView.val();
-    doc.dispatchEvent(new Event('WebComponentsReady'));
-
+    fn.storeState();
+    fn.updatePreview();
   });
+  
+  $('#reset').click(function () {
+    fn.reset();
+    fn.updatePreview();
+  });  
 
   /**/
 
   fn.updateTabs();
+  fn.restoreState();
+  fn.prepare();
+
+  window.addEventListener('load', function () {
+    fn.updatePreview();
+  });
 
 })();
